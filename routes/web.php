9cfb763f611282da5ba16279957db324c0b710e1
Ichsan <ichsan.bukit2014@gmail.com>
2022-07-19 00:37:59 +0700

    T 23:30

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DasboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,7 +62,11 @@ Route::get('/register',[RegisterController::class,'index'])->middleware('guest')
 
 Route::post('/register',[RegisterController::class,'store']);
 
-Route::get('/dashboard',[DashboardController::class,'index'])->middleware('auth');
+Route::get('/dashboard',function(){
+    return view ('dashboard.index');
+})->middleware('auth');
+
+Route::resource('dashboard/posts', DasboardPostController::class)->middleware('auth');
 
 // Route::get('/categories/{category:slug}', function (Category $category){
 //     return view ('post', [
